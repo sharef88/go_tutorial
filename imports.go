89882af7c2f74f0c565ca73a9e40bd14d6fc44a3
Andrew Wilson <Andrew Wilson>
2016-08-17 18:50:54 -0400

    9/17

--- imports.go
+++ imports.go
@@ -18,9 +18,10 @@ func split(sum int) (x, y int) {
 	return
 }
 
-var c, python, java bool
+var i, j int = 1, 2
 
 func main() {
-	var i int
-	fmt.Println(i, c, python, java)
+	var c, python, java = true, false, "no!"
+	fmt.Println(i, j, c, python, java)
+
 }

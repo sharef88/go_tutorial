62cf91170b4b791cdbd546f5104c4adeb397a54f
Andrew Wilson <Andrew Wilson>
2016-08-17 19:09:25 -0400

    13/17 explicit type conversion type(var)

--- imports.go
+++ imports.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func add(x, y int) int {
 	return x + y
@@ -17,10 +20,9 @@ func split(sum int) (x, y int) {
 }
 
 func main() {
-	var i int
-	var f float64
-	var b bool
-	var s string
-	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+	var x, y int = 3, 4
+	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(f)
+	fmt.Println(x, y, z)
 
 }

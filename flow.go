710ac8675b55e1c2ce56f396a200483b5c7ad500
Andrew Wilson <Andrew Wilson>
2016-08-22 09:46:30 -0400

    7/14 if/else/end

--- flow.go
+++ flow.go
@@ -15,9 +15,12 @@ func sqrt(x float64) string {
 func pow(x, n, lim float64) float64 {
 	if v := math.Pow(x, n); v < lim {
 		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
 	}
 	return lim
 }
+
 func main() {
 	fmt.Println(
 		pow(3, 2, 10),

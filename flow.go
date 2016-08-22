87faa76da5285f6896b80052debe52d29a139c18
Andrew Wilson <Andrew Wilson>
2016-08-22 13:33:33 -0400

    11/14 switch/case pt2

--- flow.go
+++ flow.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"runtime"
+	"time"
 )
 
 func sqrt(x float64) float64 {
@@ -23,15 +23,17 @@ func sqrt(x float64) float64 {
 }
 
 func main() {
-	fmt.Print("Go runs on ")
-	switch os := runtime.GOOS; os {
-	case "darwin":
-		fmt.Println("OS X.")
-	case "linux":
-		fmt.Println("Linux")
-	case "windows":
-		fmt.Println("Windows")
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
 	default:
-		fmt.Printf("%s.", os)
+		fmt.Println("Too far away.")
+
 	}
 }

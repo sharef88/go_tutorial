d32e4c15891391b3f5ccfc56f8d6a4918e0b8707
Andrew Wilson <Andrew Wilson>
2016-08-22 12:41:49 -0400

    Now with comments

--- flow.go
+++ flow.go
@@ -6,12 +6,15 @@ import (
 )
 
 func sqrt(x float64) float64 {
+	//z = guess
 	z := float64(100)
 
+	//Newton's method for finding the sqrt.  This is mildly obnoxious
 	step := func() float64 {
 		return z - (z*z-x)/(2*z)
 	}
 
+	//step through the equation count times
 	count := 10
 	for index := 0; index < count; index++ {
 		z = step()

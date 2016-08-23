c77d3f5462faf8c9ceec02b2d20de84b310818d9
Andrew Wilson <Andrew Wilson>
2016-08-22 21:38:02 -0400

    5/27, Some of the more advanced features of Struct

--- pointers.go
+++ pointers.go
@@ -7,9 +7,13 @@ type Vertex struct {
 	Y int
 }
 
+var (
+	v1 = Vertex{1, 2}
+	v2 = Vertex{X: 1}
+	v3 = Vertex{} //as all values default to their 0 or false value, undef -> 0
+	p  = &Vertex{1, 2}
+)
+
 func main() {
-	v := Vertex{1, 2}
-	p := &v
-	p.X = 1e9
-	fmt.Println(v)
+	fmt.Println(v1, p, v2, v3)
 }

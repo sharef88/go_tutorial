e26e20de907baada5ae28616d4aace8ccefaf7a9
Andrew Wilson <Andrew Wilson>
2016-08-25 15:50:14 -0400

    13/27, Slice of a Make, this seems like overkill

--- pointers.go
+++ pointers.go
@@ -2,14 +2,21 @@ package main
 
 import "fmt"
 
-func printSlice(s []int) {
-	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+func printSlice(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n", s, len(x), cap(x), x)
 }
 
 func main() {
-	var s []int
-	printSlice(s)
-	if s == nil {
-		fmt.Println("nil!")
-	}
+	a := make([]int, 5)
+	printSlice("a", a)
+
+	b := make([]int, 0, 5)
+	printSlice("b", b)
+
+	c := b[:2]
+	printSlice("c", c)
+
+	d := c[2:5]
+	printSlice("d", d)
+
 }

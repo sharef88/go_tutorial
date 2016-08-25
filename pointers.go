85a6c02a1f5c62748d2de440aaf320f3267c2352
Andrew Wilson <Andrew Wilson>
2016-08-25 15:36:32 -0400

    11/27 slice of a slice of a slice?

--- pointers.go
+++ pointers.go
@@ -2,16 +2,21 @@ package main
 
 import "fmt"
 
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
+
 func main() {
-	s := []int{2, 3, 5, 6, 11, 13}
+	s := []int{2, 3, 5, 7, 11, 13}
+	printSlice(s)
 
-	s = s[1:4]
-	fmt.Println(s)
+	s = s[:0]
+	printSlice(s)
 
-	s = s[:2]
-	fmt.Println(s)
+	s = s[:4]
+	printSlice(s)
 
-	s = s[1:]
-	fmt.Println(s)
+	s = s[2:]
+	printSlice(s)
 
 }

69f650f75acf45829d7eab4b13e04396474db7d4
Andrew Wilson <Andrew Wilson>
2016-08-25 12:28:09 -0400

    8/27 Slices are pointers, change slice changes array

--- pointers.go
+++ pointers.go
@@ -2,22 +2,20 @@ package main
 
 import "fmt"
 
-type Vertex struct {
-	//its a vertex declaration? x,y
-	X int
-	Y int
-}
-
-var (
-	v1 = Vertex{1, 2}
-	v2 = Vertex{X: 1}
-	v3 = Vertex{} //as all values default to their 0 or false value, undef -> 0
-	p  = &Vertex{1, 2}
-)
-
 func main() {
-	primes := [6]int{2, 3, 5, 7, 11, 13}
-	var s []int = primes[1:4]
-	fmt.Println(s)
+	names := [4]string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	fmt.Println(names)
+	a := names[0:2]
+	b := names[1:3]
+	fmt.Println(a, b)
+
+	b[0] = "XXX"
+	fmt.Println(a, b)
+	fmt.Println(names)
 
 }

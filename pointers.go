613df15c8cc06f823876ecb295a83dbc2fd0a1ac
sharef88 <andy@orbanos.org>
2016-08-26 14:55:35 -0400

    14/27 Slices can have w/e you want in them.... and vscode will auto-populate the import statement

--- pointers.go
+++ pointers.go
@@ -1,22 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func printSlice(s string, x []int) {
 	fmt.Printf("%s len=%d cap=%d %v\n", s, len(x), cap(x), x)
 }
 
 func main() {
-	a := make([]int, 5)
-	printSlice("a", a)
+	//Create Tic-tac-toe board
+	board := [][]string{
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+	}
 
-	b := make([]int, 0, 5)
-	printSlice("b", b)
+	// take some turns
+	board[0][0] = "X"
+	board[2][2] = "O"
+	board[1][2] = "X"
+	board[1][0] = "O"
+	board[0][2] = "X"
 
-	c := b[:2]
-	printSlice("c", c)
-
-	d := c[2:5]
-	printSlice("d", d)
-
+	for i := 0; i < len(board); i++ {
+		fmt.Printf("%s\n", strings.Join(board[i], " "))
+	}
 }

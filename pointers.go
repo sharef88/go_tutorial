0dda351fa4b0502269e9881422535ad1ed1ffb45
sharef88 <andy@orbanos.org>
2016-08-31 16:14:49 -0400

    19/27 Maps (python dicts)

--- pointers.go
+++ pointers.go
@@ -1,28 +1,23 @@
 package main
 
-import (
-	"fmt"
-
-	"golang.org/x/tour/pic"
-)
+import "fmt"
 
 func printSlice(s string, x []int) {
 	fmt.Printf("%s len=%d cap=%d %v\n", s, len(x), cap(x), x)
 }
 
-//Pic : Make a matrix to draw a picture
-func Pic(dx, dy int) [][]uint8 {
-	ry := make([][]uint8, dy)
-	for i := range ry {
-		ry[i] = make([]uint8, dx)
-		for j := range ry[i] {
-			val := (j ^ i)
-			ry[i][j] = uint8(val)
-		}
-	}
-	return ry
+//Vertex Type for lat and long storage
+type Vertex struct {
+	Lat, Long float64
 }
 
+var m map[string]Vertex
+
 func main() {
-	pic.Show(Pic)
+	m = make(map[string]Vertex)
+	m["Bell Labs"] = Vertex{
+		40.68433, -74.39967,
+	}
+	fmt.Println(m["Bell Labs"])
+
 }

bd9382a8676345e24384d5a6861a89718530fd11
sharef88 <andy@orbanos.org>
2016-08-31 08:47:58 -0400

    16/27 range over slice or map, returns {index, value}

--- pointers.go
+++ pointers.go
@@ -6,20 +6,10 @@ func printSlice(s string, x []int) {
 	fmt.Printf("%s len=%d cap=%d %v\n", s, len(x), cap(x), x)
 }
 
+var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+
 func main() {
-	var s []int
-	printSlice("s", s)
-
-	//append works on nil slices
-	s = append(s, 0)
-	printSlice("s", s)
-
-	// The slice grows as needed
-	s = append(s, 1)
-	printSlice("s", s)
-
-	// We can add more than one element at a time
-	s = append(s, 2, 3, 4)
-	printSlice("s", s)
-
+	for i, v := range pow {
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
 }
